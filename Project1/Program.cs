aaa47306a2d1d2f152aa1961d54d9c1660a9e09d
ghost1985 <fd5f45c2@opayq.com>
2015-04-17 12:59:44 -0500

    Updated with main function
    Added main class.

---
+++ Project1/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+
+namespace Jamaican_Beaches
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\nProgramming is a total pain in the ass");
+			Console.WriteLine("\nAlthough it feels rewarding\n");
+            Console.ReadKey();
+        }
+    }
+}
